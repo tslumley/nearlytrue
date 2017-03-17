002b02155f0b4ecabdc1189a105617c110186cb4
tslumley <t.lumley@auckland.ac.nz>
2017-03-17 15:55:07 +1300

    quadratic

--- cc-sim-quadratic.R
+++ cc-sim-quadratic.R
@@ -20,3 +20,18 @@ m2<-glm(y~x,data=sdf,family=quasibinomial,weights=wt)
 
 c(coef(summary(m0))[3,3],coef(m1)[2],coef(m2)[2])
 }
+
+
+epsilons<-c(0,.01,.03,.05,.06,.07)
+rr<-lapply(epsilons, function(b) replicate(1000,one.sim(b)))
+errors<-sapply(rr, function(d) c(var(d[3,]),var(d[2,])+(mean(d[3,])-mean(d[2,]))^2))
+
+matplot(epsilons,t(errors),pch=c(19,1),type="b",lty=1,ylim=range(0,errors),ylab="MSE")
+
+kappa<-sapply(rr, function(d) mean(d[1,]))
+
+power<-pnorm(qnorm(0.05)+pmax(0,kappa))
+
+
+matplot(epsilons,t(errors)*1000,pch=c(19,1),type="b",lty=1,ylim=range(0,errors*1000),ylab=expression("MSE"%*%1000),xaxt="n",xlab="Power")
+axis(1,at=epsilons,labels=round(100*power))
