8da6ab28fc568a72c47051ec512df067c56629a7
tslumley <t.lumley@auckland.ac.nz>
2017-03-21 14:34:46 +1300

    Linear spline for case-control

--- cc-sim-linspline.R
+++ cc-sim-linspline.R
@@ -2,10 +2,9 @@
 expit<-function(eta) exp(eta)/(1+exp(eta))
 
 
-one.sim<-function(beta=0.07,knot=1){
+one.sim<-function(beta=0.07,knot=1.8){
 df<-data.frame(x=rnorm(1e5))
-df$x2<-df$x^2
-df$mu<-expit(df$x-5+beta*pmin(df$x-knot,0))
+df$mu<-expit(df$x-5+beta*pmax(df$x-knot,0))
 df$y<-rbinom(nrow(df),1,df$mu)
 
 cases<-which(df$y==1)
@@ -14,7 +13,7 @@ controls<-sample(which(df$y==0),length(cases))
 sdf<-df[c(cases,controls),]
 sdf$wt<-ifelse(sdf$y==1,1,sum(df$y==0)/length(cases))
 
-m0<-glm(y~x+x2,data=df,family=binomial)
+m0<-glm(y~x+I(pmin(x-knot,0)),data=sdf,family=binomial)
 m1<-glm(y~x,data=sdf,family=binomial)
 m2<-glm(y~x,data=sdf,family=quasibinomial,weights=wt)
 
@@ -22,13 +21,13 @@ c(coef(summary(m0))[3,3],coef(m1)[2],coef(m2)[2])
 }
 
 
-epsilons<-c(0,.01,.03,.05,.06,.07)
-rr<-lapply(epsilons, function(b) replicate(1000,one.sim(b,knot=1)))
+epsilons<-c(0,0.2,0.3,0.35,0.4,0.6)
+rr<-lapply(epsilons, function(b) replicate(1000,one.sim(b,knot=1.8)))
 errors<-sapply(rr, function(d) c(var(d[3,]),var(d[2,])+(mean(d[3,])-mean(d[2,]))^2))
 
 matplot(epsilons,t(errors),pch=c(19,1),type="b",lty=1,ylim=range(0,errors),ylab="MSE")
 
-kappa<-sapply(rr, function(d) mean(d[1,]))
+kappa<-sapply(rr, function(d) -mean(d[1,])/sd(d[1,]))
 
 power<-pnorm(qnorm(0.05)+pmax(0,kappa))
 
